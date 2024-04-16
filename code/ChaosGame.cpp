92eeb9f39b43219cbf2a0efce66ea9eef7681628
Julian-Cto <166642280+Julian-Cto@users.noreply.github.com>
2024-04-16 14:57:54 -0700

    Changes -restart button -fixed color change rate once more dots are on the screen -point cap

--- code/ChaosGame.cpp
+++ code/ChaosGame.cpp
@@ -5,6 +5,7 @@
 #include <sstream>
 #include <vector>
 #include <ctime>
+#include <cmath>
 
 // Make code easier to type with "using namespace"
 using namespace sf;
@@ -15,9 +16,9 @@ int main()
     srand(time(0));
 
     // Create a video mode object
-	VideoMode vm(1920, 1080);
-	// Create and open a window for the game
-	RenderWindow window(vm, "Chaos Game", Style::Default);
+    VideoMode vm(1920, 1080);
+    // Create and open a window for the game
+    RenderWindow window(vm, "Chaos Game", Style::Default);
 
     // Sets font for the title along with it's specifications
     sf::Font font1;
@@ -32,15 +33,23 @@ int main()
     sf::Text instructions;
     font2.loadFromFile("DirectionsFont.ttf");
     instructions.setFont(font2);
-    instructions.setString("- Click on three points to form a triangle.\n\n- Click on a fourth point to start the chaos.");
+    instructions.setString("\n- Click on three points to form a triangle.\n\n- Click on a fourth point to start the chaos.\n\n- Click 'r' to restart.");
     instructions.setCharacterSize(25);
     instructions.setFillColor(sf::Color::White);
-
+    // Sets restarting screen
+    sf::Font font3;
+    sf::Text restarting;
+    font3.loadFromFile("DirectionsFont.ttf");
+    restarting.setFont(font3);
+    restarting.setString("restarting...");
+    restarting.setCharacterSize(25);
+    restarting.setPosition(1700.f, 1000.f);
+    restarting.setFillColor(sf::Color::White);
 
     vector<Vector2f> vertices;
     vector<Vector2f> points;
-    
-    //sets a vector of colors
+
+    //sets a vector of colors Purpose: is for logo changing colors
     vector<Color> background;
     background.push_back(Color::Magenta);
     background.push_back(Color::Blue);
@@ -49,23 +58,27 @@ int main()
     background.push_back(Color::Green);
     background.push_back(Color::Cyan);
 
+    bool restart = false;
+
     int frames = 0;
     int randColor = 0;
+    int area_of_mid_size_triangle = 109005;//line 116
+    int restartingScreen = 0;
 
-	while (window.isOpen())
-	{
+    while (window.isOpen())
+    {
         /*
-		****************************************
-		Handle the players input
-		****************************************
-		*/
+        ****************************************
+        Handle the players input
+        ****************************************
+        */
         Event event;
-		while (window.pollEvent(event))
-		{
+        while (window.pollEvent(event))
+        {
             if (event.type == Event::Closed)
             {
-				// Quit the game when the window is closed
-				window.close();
+                // Quit the game when the window is closed
+                window.close();
             }
             if (event.type == sf::Event::MouseButtonPressed)
             {
@@ -75,11 +88,11 @@ int main()
                     std::cout << "mouse x: " << event.mouseButton.x << std::endl;
                     std::cout << "mouse y: " << event.mouseButton.y << std::endl;
 
-                    if(vertices.size() < 3)
+                    if (vertices.size() < 3)
                     {
                         vertices.push_back(Vector2f(event.mouseButton.x, event.mouseButton.y));
                     }
-                    else if(points.size() == 0)
+                    else if (points.size() == 0)
                     {
                         ///fourth click
                         ///push back to points vector
@@ -89,16 +102,22 @@ int main()
             }
         }
         if (Keyboard::isKeyPressed(Keyboard::Escape))
-		{
-			window.close();
-		}
+        {
+            window.close();
+        }
+        //restarts to main menu
+        if (Keyboard::isKeyPressed(Keyboard::R))
+        {
+            restartingScreen = 1000;//starts the restarting screen to display for 1000 frames
+            restart = true;
+        }
         /*
-		****************************************
-		Update
-		****************************************
-		*/
+        ****************************************
+        Update
+        ****************************************
+        */
 
-        if(points.size() > 0)
+        if (points.size() > 0)
         {
             ///generate more point(s)
             ///select random vertex
@@ -106,59 +125,114 @@ int main()
             ///push back the newly generated coord.
             float trianglePoint;
             float newX, newY;
+            int currentArea;
+            //this next part gets the area of the current triangle 
+            currentArea = .5 * abs((vertices[0].x * (vertices[1].y - vertices[2].y) + vertices[1].x * (vertices[2].y - vertices[1].y) + vertices[2].x * (vertices[0].y - vertices[2].y)));
+            if (points.size() < 31000)
+            {
+                if (currentArea < area_of_mid_size_triangle)
+                {
 
-            trianglePoint = rand() % 3;
+                    for (int i = 0; i < 3; i++)//creates 3 dots a frame for a slower pattern generation rate
+                    {
+                        
+                        if (points.size() < 5500)//Medium size triangle and under: stops generating more dots when it his 5500 dots
+                        {
+                            trianglePoint = rand() % 3;
 
-            newX = (vertices[trianglePoint].x + points[points.size() - 1].x) / 2;
-            newY = (vertices[trianglePoint].y + points[points.size() - 1].y) / 2;
+                            newX = (vertices[trianglePoint].x + points[points.size() - 1].x) / 2;
+                            newY = (vertices[trianglePoint].y + points[points.size() - 1].y) / 2;
 
-            sf::Vector2f newPoint(newX, newY);
-            points.push_back(newPoint);
+                            sf::Vector2f newPoint(newX, newY);
+                            points.push_back(newPoint);
+                        }
+                    }
+
+                }
+                else
+                {
+                    for (int i = 0; i < 50; i++)//creats 50 dots a frame to create a bigger pattern faster
+                    {
+                        if (points.size() < 31000)//medium size triangle and over: stops generating more dots when it hits 31000 dots
+                        {
+                        trianglePoint = rand() % 3;
+
+                        newX = (vertices[trianglePoint].x + points[points.size() - 1].x) / 2;
+                        newY = (vertices[trianglePoint].y + points[points.size() - 1].y) / 2;
+
+                        sf::Vector2f newPoint(newX, newY);
+                        points.push_back(newPoint);
+                        }
+                    }
+                }
+            }
+            
 
         }
 
         /*
-		****************************************
-		Draw
-		****************************************
-		*/
+        ****************************************
+        Draw
+        ****************************************
+        */
         window.clear();
-        if (vertices.size() == 0)
+        if (restartingScreen > 0)//displays restarting screen
         {
-            //displays the title and instructions
-            title.setPosition(540.f, 300.f);
-            instructions.setPosition(600.f, 360.f);
-            window.draw(title);
-            window.draw(instructions);
+            window.draw(restarting);
+            --restartingScreen;
         }
-        else
-        {
-            //once a user clicks, sets background to the bottom right corner
-            if (frames == 0 || frames % 500 == 0)
+        else {
+            if (vertices.size() == 0)
             {
-                //color changes every 500 frames
-                randColor = rand() % 6;
+                //displays the title and instructions
+                title.setPosition(540.f, 300.f);
+                instructions.setPosition(600.f, 360.f);
+                window.draw(title);
+                window.draw(instructions);
             }
-            title.setPosition(1100.f, 1000.f);
-            //sets the color of the bottom right conrer to a random color
-            title.setFillColor(background[randColor]);
-            window.draw(title);
+            else if (restart)
+            {
+                //clears all of the points made
+                vertices.clear();
+                points.clear();
+                restart = false;
+                
+            }
+            else
+            {
+                if ((frames == 0 || frames % 500 == 0) && points.size() < 2000)//if there are less than 2000 points on the screen
+                {
+                    //color changes every 500 frames
+                    randColor = rand() % 6;
+                }
+                else if (frames % 10 == 0 && points.size() > 2000)//once there are more than 2000 points one the screen
+                {
+                    //color changes every 100 frames
+                    randColor = rand() % 6;
+                }
+                //once a user clicks, sets background to the bottom right corner
+                title.setPosition(1100.f, 1000.f);
+                //sets the color of the bottom right corner to a random color
+                title.setFillColor(background[randColor]);
+                window.draw(title);
 
-        }
-        for(int i = 0; i < vertices.size(); i++)
-        {
-            RectangleShape rect1(Vector2f(10,10));
-            rect1.setPosition(Vector2f(vertices[i].x, vertices[i].y));
-            rect1.setFillColor(Color::Blue);
-            window.draw(rect1);
-        }
-        for (int j = 0; j < points.size(); j++)
-        {
-            //displays the points after the triangle is set
-            RectangleShape rect2(Vector2f(10, 10));
-            rect2.setPosition(Vector2f(points[j].x, points[j].y));
-            rect2.setFillColor(Color::White);
-            window.draw(rect2);
+            }
+            for (int i = 0; i < vertices.size(); i++)
+            {
+                //displays triangle points
+                RectangleShape rect1(Vector2f(10, 10));
+                rect1.setPosition(Vector2f(vertices[i].x, vertices[i].y));
+                rect1.setFillColor(Color::Blue);
+                window.draw(rect1);
+            }
+            for (int j = 0; j < points.size(); j++)
+            {
+                //displays the points after the triangle is set
+                RectangleShape rect2(Vector2f(10, 10));
+                rect2.setPosition(Vector2f(points[j].x, points[j].y));
+                rect2.setFillColor(Color::White);
+                window.draw(rect2);
+            }
         }
         frames++;
         window.display();
